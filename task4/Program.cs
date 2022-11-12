6b9dcba6a8a56ebfa77601aab8123b960773a7e2
giezz <giezz@vk.com>
2022-11-13 00:35:27 +0300

    task 4 dynamic dll loading

---
+++ task4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+
+namespace task4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[,] matrixA = PopulateMatrix(100, 100);
+            int[,] matrixB = PopulateMatrix(100, 100);
+            
+            
+            Assembly a = Assembly.Load("task4Library");
+            Object o = a.CreateInstance("task4Library.Class1");
+            Type t = a.GetType("task4Library.Class1");
+            MethodInfo[] mi = t.GetMethods();
+            Console.WriteLine(mi[0].Invoke(o, new object[] {matrixA, matrixB}));
+            Console.WriteLine(mi[1].Invoke(o, null));
+            
+            matrixA = PopulateMatrix(1000, 1000);
+            matrixB = PopulateMatrix(1000, 1000);
+            
+            Console.WriteLine(mi[0].Invoke(o, new object[] {matrixA, matrixB}));
+            Console.WriteLine(mi[1].Invoke(o, null));
+        }
+
+        public static int[,] PopulateMatrix(int dim1, int dim2)
+        {
+            int[,] matrix = new int[dim1, dim2];
+            Random random = new Random();
+            for (int i = 0; i < dim1; i++)
+            {
+                for (int j = 0; j < dim2; j++)
+                {
+                    matrix[i, j] = random.Next(1000);
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ task4Library/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace task4Library
+{
+    public class Class1
+    {
+        private long multiplicationTime;
+        private Stopwatch stopwatch;
+
+        public int[,] Multiplication(int[,] a, int[,] b)
+        {
+            if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Матрицы нельзя перемножить");
+            int[,] resultMatrix = new int[a.GetLength(0), b.GetLength(1)];
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    for (int k = 0; k < b.GetLength(0); k++)
+                    {
+                        resultMatrix[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            stopwatch.Stop();
+            multiplicationTime = stopwatch.ElapsedMilliseconds;
+            
+            return resultMatrix;
+        }
+
+        public long GetTime()
+        {
+            return multiplicationTime;
+        }
+    }
+}
